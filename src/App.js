23620258fe98f69f08b0a55f10d46170d85a3f74
Salar <salar.taeb@hotmail.com>
2021-02-11 23:50:28 -0800

    importing react router and nesting elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { Domain } from '@material-ui/icons';
 import './App.scss';
 import NewClub from './components/NewClub';
 import NewUser from './components/NewUser';
 import SearchBook from './components/Search/SearchBook';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
 function App() {
   return (
+    <Router>
     <div>
       {/* <NewClub></NewClub> */}
+      
       <NewUser></NewUser>
       <SearchBook></SearchBook>
     </div>
+    </Router>
   );
 }
 
